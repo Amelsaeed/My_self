3ec3a022861a6fef3f303fd9da06cd7fbfdbd10c
mohamed <mohamed.sobhy.abdelmageed@outlook.com>
2018-10-26 13:40:31 +0200

    edit splash & add animation

--- app/src/main/java/com/example/ahmedmagdy/theclinic/activities/SplashActivity.java
+++ app/src/main/java/com/example/ahmedmagdy/theclinic/activities/SplashActivity.java
@@ -3,20 +3,39 @@ package com.example.ahmedmagdy.theclinic.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.ahmedmagdy.theclinic.R;
 
 public class SplashActivity extends AppCompatActivity {
+    ImageView myImageView;
+    Animation myAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        //get my image
+        myImageView = (ImageView) findViewById(R.id.splashlogo);
+
+        // load the animation file (my_anim)
+        myAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanime);
+        myImageView.startAnimation(myAnimation);
+
         Thread timer =new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(0);//5000
+                    Thread.sleep(5000);//5000
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
